5844f363d34a525d1a7d8499245e14398f839691
8hatch8 <yutaka.pg@gmail.com>
2022-02-24 16:11:33 +0900

    react-iconsの導入

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { FaTwitter, FaGithub } from 'react-icons/fa';
 import CoverImage from '../images/profile_cover.jpg';
 import ProfileImage from '../images/profile.jpg';
 
@@ -14,10 +15,14 @@ export const Header = () => {
             <h3 className="title-text">Engineer</h3>
             <ul className="social-icons">
               <li className="icon-link">
-                {/*TODO*/}
+                <a href="https://twitter.com/yutaka_pg">
+                  <FaTwitter color="white" size="2rem" />
+                </a>
               </li>
               <li className="icon-link">
-                {/*TODO*/}
+                <a href="https://github.com/8hatch8">
+                  <FaGithub color="white" size="2rem" />
+                </a>
               </li>
             </ul>
           </div>
